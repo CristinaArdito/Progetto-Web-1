e4b5c30d7f0f2ff985299373eb5605d81094a2f4
Kri <cristina.ardito@studenti.unicam.it>
2017-08-14 18:21:07 +0200

    Template prodotti di dashboard

--- public/js/products.js
+++ public/js/products.js
@@ -6,12 +6,14 @@ angular.module('myApp.controllers')
     var prodotti = "";
 
     for (i = 0; i < 5; i++) {
-            prodotti = prodotti + "<ul><li>Nome prodotto: Prodotto "+i+"</li>"+
-                                    "<li>Descrizione: Descrizione prodotto "+i+"</li>"+
+            prodotti = prodotti + "<div class="+"productable"+"><ul><li>Nome prodotto: Prodotto "+i+"</li>"+
                                     "<li>Prezzo prodotto: Prezzo prodotto "+i+"</li>"+
                                     "<li>Quantità rimanente: Quantità rimanente prodotto "+i+"</li>"+
-                                    "</ul><br>";
+                                    "</ul></div><div class="+"ord"+"><a class="+"reorders"+"href="+"#!/orders"+">Riordina: </a>"+
+                                    "<input class="+"num"+" type="+"number"+" min="+"0"+"></div><br>";
         }
+
+        console.log(prodotti);
 
         $scope.showProdotti = $sce.trustAsHtml(prodotti);
 
