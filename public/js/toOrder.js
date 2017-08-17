50f78bd4746643757c40b894944116273f405bdb
Kri <cristina.ardito@studenti.unicam.it>
2017-08-17 12:16:33 +0200

    Iniziato style ordini

--- public/js/toOrder.js
+++ public/js/toOrder.js
@@ -9,14 +9,14 @@ angular.module('myApp.controllers')
         var prodotti = "";
         
             for (i = 0; i < 5; i++) {
-                    prodotti = prodotti + "<div class='productable'><ul><li>Nome prodotto: Prodotto "+i+"</li>"+
+                    prodotti = prodotti + "<div class='orderproduct'><ul><li>Nome prodotto: Prodotto "+i+"</li>"+
                                             "<li>Prezzo prodotto: Prezzo prodotto "+i+"</li>"+
                                             "<li>Quantità rimanente: Quantità rimanente prodotto "+i+"</li>"+
                                             "</ul></div><div class='ord'><a class='reorders' href='#!/orders'>Riordina: </a>"+
                                             "<input name='prodotto"+i+"' class='num' type='number' min='0'></input></div><br>";
                 }
                 
-                prodotti = prodotti + "<br><button type='submit' class='ord' ng-click = 'ordinaProdotto()'>Ordina</button>";
+                prodotti = prodotti + "<br><button type='submit' class='ordbutton' ng-click = 'ordinaProdotto()'>Ordina</button>";
                 console.log(prodotti);
 
         angular.element(document.getElementById('orderForm')).append($compile(prodotti)($scope));
