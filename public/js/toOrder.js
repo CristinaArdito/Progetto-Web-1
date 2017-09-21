9395c57053b4fc63cce721112d5b4d1d8ca5cfec
Unknown <mattia.contigiani@gmail.com>
2017-09-21 17:42:00 +0200

    Modifica pagina ordine

--- public/js/toOrder.js
+++ public/js/toOrder.js
@@ -54,10 +54,11 @@ angular.module('myApp.controllers')
 
                 prodotti = prodotti + "<div class='orderproduct'><ul>"+
                                       "<li>Nome prodotto: <span id='nProd"+i+"'>"+value[i].name+"</span></li>"+
+                                      "<li>Codice:  <span id='nCod"+i+"'>"+value[i].code+"</span>"+
                                       "<li>Prezzo prodotto:"+value[i].price+"</li>"+
-                                      "<li>Quantità rimanente:"+value[i].quantity+"</li>"+
+                                      "<li>Quantità rimanente: <span id='nQ"+i+"'>"+value[i].quantity+"</li>"+
                                       "</ul></div><div class='ord'><a class='reorders' href='#!/orders'>Riordina: </a>"+
-                                      "<input name='prodotto"+i+"' class='num' type='number' min='0' value='0'></input></div><br>";
+                                      "<input id='prodotto"+i+"' class='num' type='number' min='0' value='0'></input></div><br>";
             }
         prodotti = prodotti + "<br><button type='submit' class='ordbutton' ng-click = 'ordinaProdotto()'>Ordina</button>";
 
@@ -99,38 +100,20 @@ angular.module('myApp.controllers')
    
     $scope.ordinaProdotto = function()
     {
-       var c1 = [];
-       var c2 = [];
-       var l1;
-       var k=0;
-       var i=0;
-       var nome = "prodotto"+i;
-       var nNome = "";
+       m = [];
+       var code, quantity;
 
-       do{
-        nome = "prodotto"+i;
-        nNome = "nProd"+i;
-        l1 = angular.element(document.getElementsByName(nome));
-        if(l1[0] == undefined) break;
-        else l1 = l1[0].value;
-        if(l1 != '0'){
-            c1[k] = l1;
-            c2[k] = angular.element(document.getElementById(nNome))[0].innerHTML;
-            k++;
-        }
-        i++;
-        
-       }while(angular.element(document.getElementsByName(nome)) != undefined != '0' != 0);
+       for(i=0;i<6;i++){
+          code = angular.element(document.getElementById('nCod'+i));
+          if(code == undefined) break;
+          q1 = angular.element(document.getElementById('prodotto'+i))[0].value;
 
-       data = [c1,c2];
-       $location.path("/orderSuccess");
-       $http.post("api/product/orders", {
-            'data' : JSON.stringify(data)
-       });
-    }
-}
-]
-)
+          if(parseInt(q1) > 0){
+            m.push(code[0].innerHTML,parseInt(q1));
+          }
+       }
+    }      
+}])
 .controller('orderSuccessController', ['$scope' , function($scope){
     console.log("entro in orderSuccessController");
 }]);;
