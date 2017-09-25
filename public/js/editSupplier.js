96b17083511985bec4c6a24c73746b5ac4105bfa
Kri <cristina.ardito@studenti.unicam.it>
2017-09-25 17:39:23 +0200

    Aggiunto style modifica fornitore

--- public/js/editSupplier.js
+++ public/js/editSupplier.js
@@ -9,13 +9,13 @@ function($scope, $compile, $http, $location, DataService, SupplierService) {
 
         console.log(data);
 
-        html = '<span>Nome fornitore: </span><input type="text" id="name" value="'+data.name+'"></input><br>'+
+        html = '<div id="addSupplier"><span>Nome fornitore: </span><input type="text" id="name" value="'+data.name+'"></input><br>'+
                '<span>Email: </span><input type="text" id="email" value="'+data.email+'"></input><br>'+
                '<span>Numero di Telefono: </span><input type="text" id="tel" value="'+data.ntel+'"></input><br>'+
                '<span>Via: </span><input type="text" id="via" value="'+data.via+'"></input><br>'+
                '<span>Città: </span><input type="text" id="city" value="'+data.city+'"></input><br>'+
                '<span>Codice avviamento postale (CAP): </span><input type="text" id="cap" value="'+data.cap+'"></input><br>'+
-               '<button ng-click="update()">Modifica</button>';
+               '<button ng-click="update()">Modifica</button></div>';
         
         angular.element(document.getElementById('editSupplier')).append($compile(html)($scope));
     }
