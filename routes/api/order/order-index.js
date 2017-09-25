179c67f0990412b45d4583478dd3e41a6e23cb85
Unknown <marco.ribera@studenti.unicam.it>
2017-09-25 16:42:52 +0200

    Init Order Routes

---
+++ routes/api/order/order-index.js
@@ -0,0 +1,26 @@
+// =======================
+// API ROUTES 
+// =======================
+var express = require('express');
+var bodyParser = require('body-parser');
+var product_utilities = require('./product-utilities');
+var fs = require('fs');
+var adminRoutes = express.Router(); 
+var orderRoutes = express.Router();
+module.exports = orderRoutes;
+
+orderRoutes.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+orderRoutes.use(bodyParser.json());
+// parse application/vnd.api+json as json
+orderRoutes.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+orderRoutes.post('userOrder', function(req,res){});
+
+orderRoutes.post('supplierOrder', function(req,res){});
+
+orderRoutes.get('uOrderStory', function(req,res){});
+
+orderRoutes.get('orderNumber', function(req,res){});
+
+orderRoutes.get('orderTable',function(req,res){});
